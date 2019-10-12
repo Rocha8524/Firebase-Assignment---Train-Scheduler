8d278c287e578cc9ebf30e8f92048ec46718f434
Alex Rocha <rocha8524@gmail.com>
2019-10-12 19:23:00 -0400

    Fix moment js

--- Assets/javascript/train-scheduler.js
+++ Assets/javascript/train-scheduler.js
@@ -1,73 +1,69 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyAS4X10BXV0Okw_iDAd7kVrpfCKKjdgmJg",
-    authDomain: "train-scheduler-56b12.firebaseapp.com",
-    databaseURL: "https://train-scheduler-56b12.firebaseio.com",
-    projectId: "train-scheduler-56b12",
-    storageBucket: "train-scheduler-56b12.appspot.com",
-    messagingSenderId: "1092551141428",
-    appId: "1:1092551141428:web:09b329ac3e37a04e9d4261",
-    measurementId: "G-3HCLT5D340"
-};
+// Load document ready function
+$(document).ready(function () {
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-//Create a variable to reference the database
-var database = firebase.database();
-
-//on click function for the submit button
-$("#add-train").on("click", function (event) {
-    event.preventDefault();
-
-    // Code in the logic for storing and retrieving the most recent user.
-    var trainName = $("#train-name").val().trim();
-    var trainDestination = $("#train-destination").val().trim();
-    var trainTime = $("#train-time").val().trim();
-    var trainFrequency = $("#train-frequency").val().trim()
-
-    // Change what is saved in firebase
-    var newTrain = {
-        name: trainName,
-        destination: trainDestination,
-        time: trainTime,
-        frequency: trainFrequency,
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyAS4X10BXV0Okw_iDAd7kVrpfCKKjdgmJg",
+        authDomain: "train-scheduler-56b12.firebaseapp.com",
+        databaseURL: "https://train-scheduler-56b12.firebaseio.com",
+        projectId: "train-scheduler-56b12",
+        storageBucket: "train-scheduler-56b12.appspot.com",
+        messagingSenderId: "1092551141428",
+        appId: "1:1092551141428:web:09b329ac3e37a04e9d4261",
+        measurementId: "G-3HCLT5D340"
     };
 
-    // Don't forget to provide initial data to your Firebase database.
-    database.ref().push(newTrain);
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
 
-    // Console log the values that were just pushed to the database
-    console.log(newTrain.trainName);
-    console.log(newTrain.trainDestination);
-    console.log(newTrain.trainTime);
-    console.log(newTrain.trainFrequency);
+    //Create a variable to reference the database
+    var database = firebase.database();
 
-    // Clear the form after values have been stored
-    $("#train-name").val("");
-    $("#train-destination").val("");
-    $("#train-time").val("");
-    $("#train-frequency").val("");
-});
+    //on click function for the submit button
+    $("#add-train").on("click", function (event) {
+        event.preventDefault();
 
-// Firebase watcher + initial loader HINT: .on("value")
-database.ref().on("child_added", function (childSnapshot) {
-    console.log(childSnapshot.val());
+        // Code in the logic for storing and retrieving the most recent user.
+        var trainName = $("#train-name").val().trim();
+        var trainDestination = $("#train-destination").val().trim();
+        var trainTime = $("#train-time").val().trim();
+        var trainFrequency = $("#train-frequency").val().trim()
 
-    // Store snapshot variables
-    var trainName = childSnapshot.val().name;
-    var trainDestination = childSnapshot.val().destination;
-    var trainTime = childSnapshot.val().time;
-    var trainFrequency = childSnapshot.val().frequency;
+        // Don't forget to provide initial data to your Firebase database.
+        database.ref().push({
 
-    // Processing time schedule for each train
-    var trainTimeConverter = moment(trainTime, "hh:mm").subtract(1, "years");
-    var timeDifference = moment().diff(moment(trainTimeConverter), "minutes");
-    var timeLeft = timeDifference % trainFrequency;
-    var minutesAway = trainFrequency - timeLeft;
-    var nextArrival = moment().add(minutesAway, "minutes")
-    nextArrival = moment.format("hh:mm");
+            // Change what is saved in firebase
+            name: trainName,
+            destination: trainDestination,
+            time: trainTime,
+            frequency: trainFrequency,
+        });
 
-    // Add the data into the HTML
-    $("#train-info > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" + trainFrequency + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>");
+        // Clear the form after values have been stored
+        $("#train-name").val("");
+        $("#train-destination").val("");
+        $("#train-time").val("");
+        $("#train-frequency").val("");
+    });
+
+    // Firebase watcher + initial loader HINT: .on("value")
+    database.ref().on("child_added", function (childSnapshot) {
+
+        // Store snapshot variables
+        var trainName = childSnapshot.val().name;
+        var trainDestination = childSnapshot.val().destination;
+        var trainTime = childSnapshot.val().time;
+        var trainFrequency = childSnapshot.val().frequency;
+
+        // Processing time schedule for each train
+        var trainTimeConverter = moment(trainTime, "hh:mm").subtract(1, "years");
+        var timeDifference = moment().diff(moment(trainTimeConverter), "minutes");
+        var timeLeft = timeDifference % trainFrequency;
+        var minutesAway = trainFrequency - timeLeft;
+        var nextArrival = moment().add(minutesAway, "minutes")
+        nextArrival = moment.format("hh:mm");
+
+        // Add the data into the HTML
+        $("#train-info > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" + trainFrequency + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>");
+    });
 });
